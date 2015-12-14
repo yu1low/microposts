80c014d2aee4e6bc042c1afdffeaad6f62c75666
yu1low <yu.chin827@gmail.com>
2015-12-14 14:33:39 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -18,7 +19,28 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    if @user == current_user
+    else
+      render 'show'
+    end
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "ユーザー情報を編集しました"
+      redirect_to current_user
+    else
+      render 'edit'
+    end
+  end
+  
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :location, :profile)
+  end
+  
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
---
+++ db/migrate/20151212100748_add_profiles_to_users.rb
@@ -0,0 +1,6 @@
+class AddProfilesToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :profile, :string
+    add_column :users, :location, :string
+  end
+end
