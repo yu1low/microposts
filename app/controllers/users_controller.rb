342888ae0707371dc33b664de563e427ad68a0da
yu1low <yu.chin827@gmail.com>
2015-12-17 14:48:16 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
 
   def new
@@ -18,6 +19,7 @@ class UsersController < ApplicationController
     end
   end
   
+  private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
